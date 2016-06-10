b0dc0ec5c4cac158b3fdd7f8bfd28f9776f80248
Ian Hernandez <ihernandezs@openmailbox.org>
2016-06-10 01:13:17 -0500

    Building rules for the library and main header file created.

---
+++ bdwlib/bdwlib.h
@@ -0,0 +1,17 @@
+#ifndef BDWLIB__H
+#define BDWLIB__H
+
+#include "bdw-array.h"
+#include "bdw-bytearray.h"
+#include "bdw-file.h"
+#include "bdw-list.h"
+#include "bdw-queue.h"
+#include "bdw-simple-list.h"
+#include "bdw-stack.h"
+#include "bdw-static-queue.h"
+#include "bdw-static-stack.h"
+#include "bdw-string-utils.h"
+#include "bdw-types.h"
+
+#endif
+
