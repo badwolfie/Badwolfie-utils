91d5aefb331a49ea0a8692cb672841492c0d6290
Ian Hernandez <ihernandezs@openmailbox.org>
2016-06-18 02:54:13 -0500

    Generic utilities added

---
+++ bdwlib/bdw-utils.c
@@ -0,0 +1,57 @@
+/* bdw-utils.c
+ *
+ * Copyright (C) 2016 Ian Hernandez <ihernandezs@openmailbox.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <string.h>
+#include <stdio.h>
+
+#include "bdw-utils.h"
+
+void
+bdw_utils_clear_buffer (pointer buffer, sizetype size)
+{
+  if (buffer != NULL) {
+      memset (buffer, 0x00, size);
+  }
+}
+
+sizetype
+bdw_utills_array_get_length (pointer array)
+{
+  sizetype len = 0;
+
+  if (array)
+  {
+    while (((pointer *) array)[len])
+    {
+      len++;
+    }
+  }
+
+  return len;
+}
+
+void
+bdw_utils_flush_stdin (void)
+{
+  int64 d, i;
+	d = stdin->_IO_read_end - stdin->_IO_read_ptr;
+
+	for(i = 0; i < d; i++) {
+    getchar();
+  }
+}
+
---
+++ bdwlib/bdw-utils.h
@@ -0,0 +1,62 @@
+/* bdw-utils.h
+ *
+ * Copyright (C) 2016 Ian Hernandez <ihernandezs@openmailbox.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef BDW_UTILS__H
+#define BDW_UTILS__H
+
+#if !defined (__BDWLIB_INSIDE__) && !defined (BDWLIB_COMPILATION)
+#error "Only <bdwlib.h> can be included directly."
+#endif
+
+/*    Function prototypes    */
+#ifdef __cplusplus /*    C++ support    */
+extern "C" {
+#endif
+
+#include "bdw-types.h"
+
+/**
+ * bdw_utils_clear_buffer:
+ * @buffer: Character string to be cleared.
+ *
+ * Sets every byte on @buffer to zero, clearing all its data.
+ **/
+void bdw_utils_clear_buffer (pointer buffer, sizetype size);
+
+/**
+ * bdw_utills_array_get_length:
+ * @array: Pointer to the %NULL-terminated array to be processed.
+ *
+ * Obtains the length of a %NULL-terminated array of elements.
+ *
+ * Returns: The length of the given array.
+ **/
+sizetype bdw_utills_array_get_length (pointer array);
+
+/**
+ * bdw_utils_flush_stdin:
+ *
+ * Flushes the stdin buffer so you don't have problems while reading from it.
+ **/
+void bdw_utils_flush_stdin (void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* BDW_UTILS__H */
+
